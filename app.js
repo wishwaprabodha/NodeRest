73d5d312a53a786805f361301478b8c41fe72558
wishwaprabodha <ww46673@gmail.com>
2018-09-18 23:36:22 +0530

    First Commit - Only APIS

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const mysql=require('mysql');
+
+const app=express();
+
+
+ const port=process.env.PORT || 3000;
+
+ const bookRouter=express.Router();
+ const authRouter=express.Router();
+
+ const con = mysql.createConnection({
+    host: "localhost",
+    user: "wishwa",
+    port:3306,
+    database: "newdb",
+    password: "Egxduvwz@2116"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to DB!");
+  });
+
+ authRouter.get('/authors',(req,res)=>{
+    const query = "select * from author";
+    con.query(query, function (err, result) {
+        res.send(JSON.stringify(result));
+    });
+ });
+
+ bookRouter.get('/books',(req,res)=>{
+    const query = "select * from book";
+    con.query(query, function (err, result) {
+        console.log(result.idBook);
+        res.send(result);
+    });
+ });
+
+ authRouter.get('/authors/:id', function (req, res) {
+    const id = req.params.id;
+    const query ='SELECT * FROM author WHERE idAuthor=?';
+    con.query(query, id,function (err, result) {
+        res.send(JSON.stringify(result));
+    });
+ });
+
+ bookRouter.get('/books/:id', function (req, res) {
+    const id = req.params.id;
+    const query = 'select * from book where idBook=?';
+    con.query(query, id,function (err, result) {
+        res.send(JSON.stringify(result));
+        result=result[0];
+        console.log(result.nameBook);
+    });
+ });
+
+ bookRouter.post('/books', function (req, res) {
+    let idBook = req.body.idBook;
+    let nameBook = req.body.nameBook;
+    let priceBook = req.body.priceBook;
+    let idAuthor = req.body.idAuthor;
+
+    let query="insert into book(idBook, nameBook,priceBook,idAuthor) values ('"+idBook+"',  '"+nameBook+"','"+priceBook+"','"+idAuthor+"')";
+    con.query(query,function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+ });
+
+
+ authRouter.post('/authors', function (req, res) {
+    let idAuthor = req.body.idAuthor;
+    let nameAuthor = req.body.nameAuthor;
+    let ageAuthor = req.body.ageAuthor;
+
+    let query="insert into author(idAuthor, nameAuthor,ageAuthor) values ('"+idAuthor+"',  '"+nameAuthor+"','"+ageAuthor+"')";
+    con.query(query,function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+ });
+
+ app.use('/api',bookRouter);
+ app.use('/api',authRouter);
+
+ app.listen(port,()=>{
+    console.log("Server up on PORT:"+port);
+ });
